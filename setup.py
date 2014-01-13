5a1f4834b94315efcd475b0de6db6dd6fea69298
Thomas Kent <teeks99@yahoo.com>
2014-01-13 06:43:02 -0600

    Initial Code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='tee-pipe', 
+      version='1.0',
+      py_modules=['tee'],
+      author='Thomas Kent',
+      author_email='teeks99@yahoo.com',
+      url='http://github.com/teeks99/tee-pipe',
+      description='Allows a user to do the equilivent of POSIX tee on a pipe'
+      )
